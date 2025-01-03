6764888d84acb89ba28441d1bf72278f565904ba
Anjon Roy <anjon782446@gmail.com>
2025-01-03 21:46:47 +0530

    dependency inversion principle

--- src/com/lld/dependencyInversion/CacheStore.java
+++ src/com/lld/dependencyInversion/CacheStore.java
@@ -1,2 +1,15 @@
-package com.lld.dependencyInversion;public class CacheStore {
+package com.lld.dependencyInversion;
+
+public class CacheStore {
+    public void addKey(String key, String value){
+        //implemented as per initial requirement, but might have to be changed
+    }
+
+    public void removeKey(String key){
+        //implemented as per initial requirement, but might have to be changed
+    }
+
+    public void EvictKey(){
+        //implemented as per initial requirement, but might have to be changed
+    }
 }
